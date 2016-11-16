11f649c3dc3260a7183f1b40f96e202a9aa8154d
anand <anandhjoshi@outlook.com>
2016-11-17 01:25:51 +0530

    Updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='property',
-      version='2.0',
+      version='2.1',
       packages=['properties'],
       description='A python module to read a property file',
       author='Anand Joshi',
