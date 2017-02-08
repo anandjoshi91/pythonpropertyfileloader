ea4dd4e8cef515709ea96a15f071bec9b33a4a4b
anand <anandhjoshi@outlook.com>
2017-02-08 21:23:33 +0900

    Fix bug for  "=" separator.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='property',
       version='2.2',
       packages=['properties'],
-      description='A python module to read a property file.\n Similar to PropertyPlaceholderConfigurer in spring (java) \n Example : http://anandjoshi.me/pythonpropertyfileloader/',
+      description='A python module to read a property file.Similar to PropertyPlaceholderConfigurer in spring (java).Example : http://anandjoshi.me/pythonpropertyfileloader/',
       author='Anand Joshi',
       author_email='anandhjoshi@outlook.com',
       )
