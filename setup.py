2d74647a799c1b8be2411391d0b0b8316268825b
Anand Joshi <anandhjoshi@outlook.com>
2023-02-12 18:18:14 +0900

    update project info

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
 setup(name='property',
-      version='2.2',
+      version='2.4',
       packages=['properties'],
       description='A python module to read a property file.Similar to PropertyPlaceholderConfigurer in spring (java).Example : http://anandjoshi.me/pythonpropertyfileloader/',
       author='Anand Joshi',
       author_email='anandhjoshi@outlook.com',
+      keywords= ["property", "read-property-file", "property-interpolation"],
+      license='MIT',
+      download_url='https://github.com/anandjoshi91/pythonpropertyfileloader/archive/refs/tags/2.4.zip'
       )
