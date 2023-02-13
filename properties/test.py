18896cb8c1608bd3fc4b6fb2c0fcf789d99d24a2
Angelo Turetta <aturetta@gmail.com>
2023-02-13 14:52:16 +0400

    feat: resolve env vars while substituting defaults to false for backward compatibility collect missing keys instead of throwing exception

--- properties/test.py
+++ properties/test.py
@@ -1,9 +1,26 @@
-from properties.p import Property
+from p import Property
+from pathlib import Path
+import os
+import sys
 
-prop = Property()
-dic_prop = prop.load_property_files('my_file.properties')
+__use_env = True
+
+for arg in sys.argv[1:]:
+    if arg == "env":
+        __use_env=True
+    elif arg == "noenv":
+        __use_env=False
+
+prop = Property(use_env=__use_env)
+testfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'my_file.properties')
+dic_prop = prop.load_property_files(testfile)
 
 print(dic_prop)
+
+if mis := prop.get_missing_values() :
+    print("The following unknown keys where detected while finding substitutions:")
+    for k in mis.keys() :
+        print(k)
 
 # Output
 
