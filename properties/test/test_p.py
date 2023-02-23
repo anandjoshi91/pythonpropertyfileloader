f39be0275b8860f9bd0c916bbe71dc343cb4a805
Anand Joshi <anandhjoshi@outlook.com>
2023-02-23 19:44:40 +0900

    add more tests

--- properties/test/test_p.py
+++ properties/test/test_p.py
@@ -4,8 +4,9 @@ import sys
 import pytest
 
 
-def get_test_file():
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'my_file.properties')
+def get_test_file(file = 'my_file.properties'):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), file)
+
 
 def test_simple_prop_reader():
     prop = Property()
@@ -26,12 +27,14 @@ def test_prop_reader_with_env():
     os.environ['HOME'] = '/home/user'
     dic_prop = prop.load_property_files(get_test_file())
     assert dic_prop.get('user_dir') == '/home/user/test'
+    os.environ['HOME'] = ''
 
 def test_prop_reader_with_env_override():
     prop = Property(use_env=True)
     os.environ['bar'] = 'ice-cream-bar'
     dic_prop = prop.load_property_files(get_test_file())
     assert dic_prop.get('bar') == 'fudge-bar'
+    os.environ['bar'] = ''
 
 def test_prop_reader_with_missing_vals():
     prop = Property()
@@ -39,3 +42,13 @@ def test_prop_reader_with_missing_vals():
     assert len(prop.get_missing_values()) == 2
     assert 'HOME' in prop.get_missing_values()
     assert 'id' in prop.get_missing_values()
+
+def test_prop_reader_with_multiple_files():
+    prop = Property()
+    dic_prop = prop.load_property_files(get_test_file('test-files/test1.prop'), get_test_file('test-files/test2.properties'))
+    assert dic_prop.get('app') == 'test-app-2.5'
+
+def test_prop_reader_with_custom_separator():
+    prop = Property(assign_token='=>')
+    dic_prop = prop.load_property_files(get_test_file('test-files/test3.properties'))
+    assert dic_prop.get('foo') == 'bar'
